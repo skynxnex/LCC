ef726894598eaa129166ba9e25c4132e011f6432
Mackingzie <mackingzie@gmail.com>
2012-02-02 11:13:54 +0100

    refaktoring

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
 
 	var game = {
-		gameStatus : 0,
+		gamestatus : 0,
 		tilesLeft : {
 			tiles : 48
 		}
@@ -13,107 +13,73 @@ $(document).ready(function() {
 	}
 
 	game.init = function() {
-		game.gameStatus = 1;
+		game.gamestatus = 1;
+		console.log('game is active');
 	}
 
 	game.end = function() {
-		game.gameStatus = 0;
+		game.gamestatus = 0;
+		console.log('game has ended');
 
 	}
 
-	game.strStatus = function() {
-		if(game.gameStatus == 1) {
-			return 'Game is active';
-		} else {
-			return 'Game ended';
 
-		}
-
-	}
-	game.status = function() {
-		return game.gameStatus;
-	}
 	var player = {
 		name : '',
 		score : '',
 		decktiles : {}
 	}
-
-	player1.setPlayerName = function(name, player) {
-		if(player == 1) {
-			player1.name = name;
-		} else {
-			player2.name = name;
-		}
-	}
-	playfield = {
-		playfield : {
-			'pf1' : '',
-			'pf2' : '',
-			'pf3' : '',
-			'pf4' : '',
-		}
-
+	var player1 = player;
+	var player2 = player;
+	
+	var playfield = {
+		// playfield : {
+		// 'pf1' : '',
+		// 'pf2' : '',
+		// 'pf3' : '',
+		// 'pf4' : '',
+		// },
+		activePlayfield : null
 	}
 
-	playfield.setPlayfield = function(playfield) {
 
-	}
 	//Game
-
-	$('#newGame').click(function() {
-
-		if(game.status() == 0) {
-			$('#resumeGame').hide();
-
+	console.log(game.gamestatus);
+	
+	$('a #new').click(function() {
+		if(game.gamestatus == 0) {
+			$('#resume').hide();
 			game.init();
-			console.log(game.strStatus());
-
-			//end game
-			game.end();
-			console.log(game.strStatus());
-
-		} else {
-			$('#resumeGame').show();
+		} else if(game.gamestatus == 1) {
+			$('#resume').show();
 			console.log('continue old game?');
-		}
+	}
+		
 	});
+	
+	
+	//end game
+	$('a #end').click(function() {
+		console.log(game.strStatus());
+		game.end();
+	});
+	
+	
 	//set playfield
-	$("#newPlayfield").click(function() {
-		var selPlayfield = '';
-		var testp = $('#newPlayfield a').val();
-		console.log(testp);
-
-		switch (selPlayfield) {
-			case '#pf1' :
-				selPlayfield = 'pf1';
-				break;
-			case '#pf2' :
-				selPlayfield = 'pf2';
-				break;
-			case '#pf3' :
-				selPlayfield = 'pf3';
-				break;
-			case '#pf4' :
-				selPlayfield = 'pf4';
-				break;
-		}
-
-		playfield.setPlayfield(selPlayfield);
+	$(".individual a").click(function() {
+		var selPlayfield = $(this).attr('id');
+		playfield.activePlayfield = selPlayfield;
+		console.log('activeplay field is ' +playfield.activePlayfield);
 	})
 	//set player
 	$("#playerContinue").click(function() {
-
-		if(!empty(playfield.playfield)) {
-			var player1 = player;
-			var player2 = player;
-
-			player.setPlayerName($('#player1').val(), 1);
-			player.setPlayerName($('#player2').val(), 2);
+		
+			player1.name= $('#playerOneName').val();
+			player2.name = $('#playerTwoName').val();
+		
+	
 			console.log(player1.name);
 			console.log(player2.name);
-		} else {
-			console.log('no playfield selected');
-		}
+	
 	});
 });
